567108b543fbbdab9b2da83002d3d6d24178b5d8
thalexbr <81378630+thalexbr@users.noreply.github.com>
2021-04-01 19:41:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Thales Gomes - 6DVP')
+  res.send('Hello World! Thales Gomes - 6DVP - Testando pipeline')
 })
 
 app.listen(port, () => {
